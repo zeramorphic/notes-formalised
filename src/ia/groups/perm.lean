5327eb3d4f7a2723924b54f49f54e3b561aee875
thirdsgames <thirdsgames2018@gmail.com>
2022-05-29 00:33:03 +0100

    Permutations
    Signed-off-by: thirdsgames <thirdsgames2018@gmail.com>

--- src/ia/groups/perm.lean
+++ src/ia/groups/perm.lean
@@ -1,4 +1,5 @@
 import data.list.cycle
+import data.nat.modeq
 import ia.groups.hom
 
 namespace notes
@@ -137,8 +138,9 @@ end
 
 variable {n : ℕ}
 
-def sym_fin.order (σ : sym_fin n) (x : fin n) : ℕ :=
-@nat.find (λ n, 0 < n ∧ (σ ^ n) x = x) _ begin
+lemma sym_fin.ex_order (σ : sym_fin n) (x : fin n) :
+∃ (n_1 : ℕ), (λ (n_1 : ℕ), 0 < n_1 ∧ (σ ^ n_1) x = x) n_1 :=
+begin
   obtain ⟨a, b, hne, hab⟩ := fintype.exists_ne_map_eq_of_infinite (λ (n : ℕ), (σ ^ n) x),
   wlog : a < b,
   { exact ne.lt_or_lt hne },
@@ -155,8 +157,85 @@ def sym_fin.order (σ : sym_fin n) (x : fin n) : ℕ :=
   exact equiv.injective _ hab
 end
 
+def sym_fin.order (σ : sym_fin n) (x : fin n) : ℕ :=
+@nat.find (λ n, 0 < n ∧ (σ ^ n) x = x) _ (sym_fin.ex_order σ x)
+
+@[simp]
+lemma sym_fin.order_pos (σ : sym_fin n) (x : fin n) : 0 < σ.order x :=
+by { unfold sym_fin.order, simp }
+
+@[simp]
+lemma sym_fin.pow_order (σ : sym_fin n) (x : fin n) : (σ ^ σ.order x) x = x :=
+(nat.find_spec (sym_fin.ex_order σ x)).right
+
+@[simp]
+lemma sym_fin.pow_pow_order (σ : sym_fin n) (x : fin n) (n : ℕ) :
+((σ ^ σ.order x) ^ n) x = x :=
+begin
+  induction n,
+  { simp },
+  { rw nat.succ_eq_add_one,
+    rw nat.add_comm,
+    rw group.add_nat_pow,
+    simp,
+    rw n_ih,
+    exact sym_fin.pow_order σ x }
+end
+
 def sym_fin.orbit_of (σ : sym_fin n) (x : fin n) : list (fin n) :=
 list.map (λ n, (σ ^ n) x) $ list.range (sym_fin.order σ x)
+
+@[simp]
+lemma sym_fin.orbit_length_eq_order (σ : sym_fin n) (x : fin n) :
+(σ.orbit_of x).length = σ.order x :=
+by { unfold sym_fin.orbit_of, simp }
+
+@[simp]
+lemma sym_fin.orbit_of_nth (σ : sym_fin n) (x : fin n) (k : ℕ) :
+(σ.orbit_of x).nth k = if k < σ.order x then some ((σ ^ k) x) else none :=
+begin
+  split_ifs,
+  { unfold sym_fin.orbit_of,
+    simp,
+    exact ⟨k, list.nth_range h, rfl⟩ },
+  { rw list.nth_eq_none_iff,
+    simp,
+    exact not_lt.mp h }
+end
+
+lemma sym_fin.eq_pow_of_mem_orbit_of {σ : sym_fin n} {x y : fin n}
+(h : y ∈ σ.orbit_of x) : ∃ (n : ℕ), n < σ.order x ∧ (σ ^ n) x = y :=
+begin
+  unfold sym_fin.orbit_of at h,
+  simp at h,
+  exact h
+end
+
+@[simp]
+lemma sym_fin.mem_orbit_of_ext (σ : sym_fin n) (x y : fin n) :
+y ∈ σ.orbit_of x ↔ ∃ (n : ℕ), (σ ^ n) x = y :=
+begin
+  unfold sym_fin.orbit_of,
+  simp,
+  split,
+  { rintro ⟨a, ha₁, ha₂⟩,
+    exact ⟨a, ha₂⟩ },
+  { rintro ⟨a, ha⟩,
+    refine ⟨a % σ.order x, _, _⟩,
+    { apply nat.mod_lt,
+      simp },
+    { rw ← nat.div_add_mod a (σ.order x) at ha,
+      rw ← ha,
+      rw nat.add_comm,
+      rw group.add_nat_pow,
+      simp,
+      rw ← group.nat_pow_pow,
+      simp } }
+end
+
+@[simp]
+lemma sym_fin.mem_orbit_of_self (σ : sym_fin n) (x : fin n) : x ∈ σ.orbit_of x :=
+by { simp, exact ⟨0, rfl⟩ }
 
 @[simp]
 lemma sym_fin.orbit_of_nodup (σ : sym_fin n) (x : fin n) : (sym_fin.orbit_of σ x).nodup :=
@@ -191,7 +270,143 @@ end
 def sym_fin.cycle_of (σ : sym_fin n) (x : fin n) : cycle (fin n) :=
 ↑(sym_fin.orbit_of σ x)
 
+@[simp]
+lemma sym_fin.mem_cycle_of (σ : sym_fin n) (x y : fin n) :
+y ∈ σ.cycle_of x ↔ y ∈ σ.orbit_of x := iff.rfl
+
+@[simp]
+lemma sym_fin.mem_cycle_of_self (σ : sym_fin n) (x : fin n) : x ∈ σ.cycle_of x :=
+by simp
+
+@[simp]
+lemma sym_fin.cycle_of_nodup (σ : sym_fin n) (x : fin n) : (sym_fin.cycle_of σ x).nodup :=
+by { unfold sym_fin.cycle_of, simp }
+
 def sym_fin.cycle_perm_of (σ : sym_fin n) (x : fin n) : cycle_perm (fin n) :=
-⟨sym_fin.cycle_of σ x, by { unfold sym_fin.cycle_of, simp }⟩
+⟨sym_fin.cycle_of σ x, sym_fin.cycle_of_nodup σ x⟩
+
+@[simp]
+lemma sym_fin.mem_cycle_perm_of (σ : sym_fin n) (x y : fin n) :
+y ∈ (σ.cycle_perm_of x).cy ↔ y ∈ σ.cycle_of x := iff.rfl
+
+@[simp]
+lemma sym_fin.cycle_perm_next_eq_cycle_next (σ : sym_fin n) (x y : fin n)
+(hmem : y ∈ σ.cycle_of x) :
+(σ.cycle_perm_of x).cy.next (sym_fin.cycle_of_nodup σ x) y ((sym_fin.mem_cycle_perm_of σ x y).mp hmem) =
+(σ.cycle_of x).next (sym_fin.cycle_of_nodup σ x) y hmem :=
+by unfold sym_fin.cycle_perm_of
+
+@[simp]
+lemma sym_fin.mem_cycle_perm_of_self (σ : sym_fin n) (x : fin n) : x ∈ (σ.cycle_perm_of x).cy :=
+by simp
+
+lemma quotient.hrec_on'_eq_hrec_on {α : Sort u_1} [s₁ : setoid α] {φ : quotient s₁ → Sort*}
+(qa : quotient s₁) (f : Π (a : α), φ (quotient.mk' a)) (c : ∀ (a₁ a₂ : α), a₁ ≈ a₂ → f a₁ == f a₂)
+: quotient.hrec_on' qa f c = quotient.hrec_on qa f c := rfl
+
+lemma cycle_next_eq_list_next {α : Type*} [decidable_eq α] (s : list α) (hs : s.nodup) (x : α) (hx : x ∈ s) :
+s.next x hx = (↑s : cycle α).next hs x hx := by refl
+
+lemma range_eq_cons_cons (a : ℕ) : ∃ ys, list.range (a + 2) = 0 :: 1 :: ys :=
+begin
+  induction a,
+  { have : list.range 2 = [0, 1] := rfl,
+    rw this,
+    exact ⟨[], rfl⟩ },
+  { obtain ⟨ys, hys⟩:= a_ih,
+    refine ⟨ys ++ [a_n + 2], _⟩,
+    rw list.range_succ,
+    rw hys,
+    rw list.cons_append,
+    rw list.cons_append }
+end
+
+@[simp]
+lemma sym_fin.cycle_perm_act (σ : sym_fin n) (x : fin n) : sym_fin.cycle_perm_of σ x x = σ x :=
+begin
+  rw cycle_perm.act_def,
+  rw cycle_perm.next_if_mem_of_mem (sym_fin.mem_cycle_perm_of_self σ x),
+  have := sym_fin.cycle_perm_next_eq_cycle_next σ x x (sym_fin.mem_cycle_of_self σ x),
+  convert this,
+  unfold sym_fin.cycle_of,
+  unfold sym_fin.orbit_of,
+  convert cycle_next_eq_list_next (list.map (λ (n_1 : ℕ), (σ ^ n_1) x) (list.range (σ.order x))) _ _ _,
+  by_cases σ.order x ≤ 1,
+  { have ord_one := ge_antisymm (nat.succ_le_of_lt $ sym_fin.order_pos σ x) h,
+    suffices : list.map (λ (m : ℕ), (σ ^ m) x) (list.range (σ.order x)) = [x],
+    { simp_rw this,
+      rw list.next_singleton x x,
+      obtain ⟨_, hord⟩ := nat.find_spec (sym_fin.ex_order σ x),
+      unfold sym_fin.order at ord_one,
+      rw ord_one at hord,
+      simp at hord,
+      exact hord },
+    rw ord_one,
+    have : list.range 1 = [0] := rfl,
+    rw this,
+    simp },
+  { obtain ⟨k, hk⟩ : ∃ (k : ℕ), σ.order x = k.succ.succ,
+    { obtain ⟨k₁, hk₁⟩ := nat.exists_eq_succ_of_ne_zero (_ : σ.order x ≠ 0),
+      obtain ⟨k₂, hk₂⟩ := nat.exists_eq_succ_of_ne_zero (_ : k₁ ≠ 0),
+      refine ⟨k₂, _⟩,
+      rw hk₁, rw hk₂,
+      { intro hzero,
+        rw hzero at hk₁,
+        rw hk₁ at h,
+        exact h (le_refl 1) },
+      { intro hzero,
+        rw hzero at h,
+        exact h (zero_le 1) } },
+    obtain ⟨ys, hys⟩ := range_eq_cons_cons k,
+    rw ← nat.succ_eq_add_one at hys,
+    rw ← nat.succ_eq_add_one at hys,
+    rw ← hk at hys,
+    simp_rw hys,
+    simp,
+    rw list.next_cons_cons_eq _ x (σ x) }
+end
+
+@[simp]
+lemma sym_fin.cycle_perm_eq_of_mem_orbit (σ : sym_fin n) (x y : fin n)
+(horb : y ∈ σ.orbit_of x) : σ.cycle_perm_of x = σ.cycle_perm_of y :=
+begin
+  unfold sym_fin.cycle_perm_of,
+  unfold sym_fin.cycle_of,
+  simp,
+  rw list.is_rotated_iff_mod,
+  obtain ⟨k, hk₁, hk₂⟩ := sym_fin.eq_pow_of_mem_orbit_of horb,
+  refine ⟨k, _, _⟩,
+  { unfold sym_fin.orbit_of,
+    simp,
+    exact le_of_lt hk₁ },
+  { ext a a',
+    simp,
+    by_cases case : a < (σ.orbit_of x).length,
+    { rw list.nth_rotate case,
+      simp at case ⊢,
+      have : (a + k) % σ.order x < σ.order x,
+      { apply nat.mod_lt,
+        simp },
+      have : a < σ.order y,
+      { sorry },
+      split_ifs,
+      suffices : (σ ^ ((a + k) % σ.order x)) x = (σ ^ a) y,
+      { rw this },
+      have rw_ak : (↑a + ↑k : ℤ) % (σ.order x) = (↑a + ↑k) - ↑(σ.order x) * ((↑a + ↑k) / (σ.order x))
+      := int.mod_def (↑a + ↑k) ↑(sym_fin.order σ x),
+      rw ← group.pow_int_coe,
+      rw int.coe_nat_mod,
+      rw int.coe_nat_add,
+      rw rw_ak,
+      rw int.sub_eq_add_neg,
+      rw int.add_comm,
+      rw group.add_int_pow,
+      rw ← int.coe_nat_add,
+      rw group.pow_int_coe,
+      rw group.add_nat_pow,
+      simp,
+      rw hk₂, }
+     }
+end
 
 end notes
